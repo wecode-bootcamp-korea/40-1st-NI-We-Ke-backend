5fa34c08f3b107cac9be6082099406bdc7b75066
aaa aaa <aaaaaa@aaas-MacBook-Pro.local>
2022-12-07 11:27:53 +0900

    [ADD] .env.sample 파일 수정 , app.js에 미들웨어 추가 ,appDataSource instance 생성

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
- 
-app.use(cors())
- 
-app.get('/ping', function (req, res, next) {
-  res.json({message: 'pong'})
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const { AppDataSource } = require("./src/models/data-source")
+const { routes } = require("./src/routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+app.use(routes);
+
+
+app.get('/ping'  , function (req,res ,next){
+  res.json({message :'pong'})
 })
- 
-app.listen(3000, function () {
-  console.log('server listening on port 3000')
-})
+
+const startServer = async () => {
+  const PORT = process.env.PORT;
+
+  await AppDataSource.initialize();
+
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+  });
+};
+
+startServer();
+
