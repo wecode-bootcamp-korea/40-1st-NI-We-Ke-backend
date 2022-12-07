6670d7c4b4b3f6f2b0f73bd4f9f15ced2fec2752
aaa aaa <aaaaaa@aaas-MacBook-Pro.local>
2022-12-07 23:37:39 +0900

    [ADD]signup,signin 기능 추가

---
+++ src/models/user.dao.js
@@ -0,0 +1,31 @@
+const { appDataSource } = require("./data-source");
+
+const createUser = async (email, password) => {
+  await appDataSource.query(
+    `
+    INSERT INTO users (
+      email,
+      password
+    ) VALUES (
+      ?,
+      ?
+    )
+    `,
+    [email, password]
+  );
+};
+
+const getUserByEmail = async (email) => {
+  const [user] = await appDataSource.query(
+    `
+      SELECT *
+      FROM users u
+      WHERE u.email = ?
+    `,
+    [email]
+  );
+
+  return user;
+};
+
+module.exports = { createUser, getUserByEmail };
