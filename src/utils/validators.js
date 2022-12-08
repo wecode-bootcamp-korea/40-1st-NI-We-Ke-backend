f4ac4f3f4118cdf3e9f6aba24ebcb412eae74add
aaa aaa <aaaaaa@aaas-MacBook-Pro.local>
2022-12-08 16:28:14 +0900

    [ADD]비밀번호 정규식 조건 변경

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,9 +1,9 @@
 const validateEmail = (email) => {
-    const re = new RegExp(
+    const EMAIL_REG = new RegExp(
       /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
     );
   
-    if (!re.test(email)) {
+    if (!EMAIL_REG.test(email)) {
       const err = new Error("invalid email");
       err.statusCode = 400;
       throw err;
@@ -11,11 +11,11 @@ const validateEmail = (email) => {
   };
 
   const validatePassword = (password) => {
-    const rez = new RegExp(
-      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
+    const PASSWORD_REG = new RegExp(
+      /"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/
     );
   
-    if (!rez.test(password)) {
+    if (!PASSWORD_REG.test(password)) {
       const err = new Error("invalid password");
       err.statusCode = 400;
       throw err;
