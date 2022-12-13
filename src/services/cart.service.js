e1d039f5b5d8e8169aa1f8838575364d40ca723d
JINJOO PARK <lullu@JINJOOui-MacBookPro.local>
2022-12-13 20:52:53 +0900

    [40기 박진주] FIX createCart 코드 수정

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,11 +1,11 @@
 const cartDao = require("../models/cart.Dao");
 
 const createCart = async (productoptionId, quantity, userId) => {
-  const carts = cartDao.getCartByUserIdProductOptionId(productoptionId, userId);
+  // //const carts = cartDao.getCartByUserIdProductOptionId(productoptionId, userId);
 
-  if (carts) {
-    // 수량을 업데이트하고 return 해주는 로직
-  }
+  // if (carts) {
+  //   // 수량을 업데이트하고 return 해주는 로직
+  // }
 
   return await cartDao.createCart(productoptionId, quantity, userId);
 };
