65d546004732c50cbd64011c6d9a6f1da017bff9
aaa aaa <aaaaaa@aaas-MacBook-Pro.local>
2022-12-15 16:27:37 +0900

    [MODIFY]카멜 케이스 수정완료

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -2,7 +2,6 @@ const express = require('express')
 
 const orderController = require('../controllers/order.controllers');
 const { tokenRequired } = require('../utils/auth');
-
 const orderRouter = express.Router();
 
 orderRouter.post("" ,tokenRequired, orderController.createOrder);
