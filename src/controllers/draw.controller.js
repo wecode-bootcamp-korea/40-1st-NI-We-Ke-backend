584dfd0ea410654b4c275cef705a43957ddde06c
JINJOO PARK <lullu@JINJOOui-MacBookPro.local>
2022-12-15 16:31:35 +0900

    [40기 박진주] draw (응모) 제품 조회기능 구현 중

--- src/controllers/draw.controller.js
+++ src/controllers/draw.controller.js
@@ -3,6 +3,7 @@ const drawService = require("../services/draw.service");
 const getDrawByProductDrawColumn = async (req, res) => {
   try {
     const draw = req.params.id;
+    console.log(draw);
 
     if (!draw) {
       throw new Error("Key Error");
--- src/models/draw.dao.js
+++ src/models/draw.dao.js
@@ -6,7 +6,14 @@ const getDrawByProductDrawColumn = async (draw) => {
     SELECT
         p.id,
         p.name productName, 
-        c.name 
+        c.name,
+        (
+          SELECT
+           image_url
+          FROM product_option_images
+          WHERE product_option_id = po.id
+          LIMIT 1
+       ) image_url
     FROM products p
     JOIN categories c ON c.id = p.category_id
     WHERE p.draw = ${draw}
